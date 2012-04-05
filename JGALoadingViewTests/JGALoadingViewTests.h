2090c9591e5b6f3ddc69d9ac7f53e759eb49d876
John Grant <johnkgrant@gmail.com>
2012-04-04 20:48:07 -0400

    reorganize

---
+++ JGALoadingViewTests/JGALoadingViewTests.h
@@ -0,0 +1,13 @@
+//
+//  JGALoadingViewTests.h
+//  JGALoadingViewTests
+//
+//  Created by John Grant on 12-04-04.
+//  Copyright (c) 2012 Healthcare Made Simple. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JGALoadingViewTests : SenTestCase
+
+@end
---
+++ sample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JGALoadingView
+//
+//  Created by John Grant on 12-04-04.
+//  Copyright (c) 2012 Healthcare Made Simple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
